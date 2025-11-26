92526cd4661714127b52dd98d6f3b6d9ae0a3703
Amritsekhon25 <62329941+Amritsekhon25@users.noreply.github.com>
2025-11-26 12:07:16 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -13,13 +13,18 @@ resource "aws_s3_bucket" "this" {
     }
   }
 
-  block_public_acls       = true
-  block_public_policy     = true
-  ignore_public_acls      = true
-  restrict_public_buckets = true
-
   tags = {
     Name        = var.s3_bucket_name
     Environment = "Dev"
   }
+}
+
+# 🔐 Block public access (new way for provider v5+)
+resource "aws_s3_bucket_public_access_block" "this" {
+  bucket = aws_s3_bucket.this.id
+
+  block_public_acls       = true
+  ignore_public_acls      = true
+  block_public_policy     = true
+  restrict_public_buckets = true
 }
